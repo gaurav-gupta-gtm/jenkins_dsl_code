99a9c246ae3713016b11685c1737becc8599d8ac
Gaurav Gupta <gauravguptagtm@gmail.com>
2020-07-02 14:54:47 +0530

    fix code

--- code.groovy
+++ code.groovy
@@ -1,6 +1,29 @@
-job('example') {
-    description('My first job')
+job('job1-deploy-web-server') {
+    description('This job is for deploy the web-server')
     steps {
-        shell('sh /jobcode/code.sh')
+        shell('sh /mycode/code.sh')
     }
+}
+job('job2-test_code') {
+    description('This job is for test the code and sent notification to developer')
+    triggers {
+        scm('@daily')
+        upstream {
+            upstreamProjects('job1-deploy-web-server')
+            threshold('SUCCESS')
+        }
+    }
+    steps {
+        shell('sh /mycode/test_code.sh')
+    }
+}
+buildPipelineView('Task 6') {
+    filterBuildQueue()
+    filterExecutors()
+    title('Project for deploy web-server')
+    displayedBuilds(5)
+    selectedJob('job1-deploy-web-server')
+    alwaysAllowManualTrigger()
+    showPipelineParameters()
+    refreshFrequency(60)
 }
